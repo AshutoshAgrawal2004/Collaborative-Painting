1ba12b34ab8fad24a4f881b39d8cf35ffde1e992
AshutoshAgrawal2004 <agrawalnidhi635@gmail.com>
2019-07-27 16:22:26 +0530

    Paints stays for 5minutes without getting deleted

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ console.log('listening at ' + port);
 var socket = require('socket.io');
 var io = socket(server);
 io.sockets.on('connection', newConnection);
+var currentpaint = [];
+setInterval(() => {
+  currentpaint = [];
+}, 60 * 5 * 1000);
 
 function newConnection(socket) {
   console.log(socket.id);
+  for(cp of currentpaint) {
+    socket.emit('sentpaint', cp);
+  }
   socket.on('painting', data => {
     console.log(data);
-    socket.broadcast.emit('sentpaint', data)
+    socket.broadcast.emit('sentpaint', data);
+    currentpaint.push(data);
   });
 }
